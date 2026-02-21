00f4dd213b5b4c6caff44c76070dc8de5e242974
unknown <y.sirajsani@umi.ac.ma>
2026-02-21 01:53:30 +0000

    implement data provider

---
+++ src/constants/index.ts
@@ -0,0 +1,9 @@
+export const DEPARTEMENTS =[
+    'CS' ,
+    'Math' ,
+    'English' ,
+];
+export const DEPARTEMENT_OPTIONS = DEPARTEMENTS.map((dept) =>({
+    value: dept,
+    label: dept,
+}));
