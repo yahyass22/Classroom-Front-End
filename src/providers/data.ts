5b11f1f2ac8a1a808ce9ff1420186e0bb4ab0157
yahyass22 <yahyasd42@gmail.com>
2026-02-26 05:04:14 +0000

    add toast for rate limitting and fixing frontend display bug

--- src/providers/data.ts
+++ src/providers/data.ts
@@ -2,8 +2,6 @@ import {createDataProvider, CreateDataProviderOptions} from "@refinedev/rest";
 
 import {BACKEND_BASE_URL} from "@/constants";
 import {ListResponse} from "@/types";
-import {Connect} from "vite";
-import NextFunction = Connect.NextFunction;
 import {HttpError} from "@refinedev/core";
 const buildHttpError = async ( response: Response):Promise<HttpError> => {
     let message = 'Request failed.';
@@ -47,7 +45,6 @@ const options: CreateDataProviderOptions = {
       return payload.data ?? [];
     },
     getTotalCount: async (response) => {
-        if (!response.ok) throw await buildHttpError(response);
       const payload: ListResponse = await response.clone().json();
       return payload.pagination?.total ?? payload.data?.length ?? 0;
     },
--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,15 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  optimizeDeps: {
+    force: true,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  ssr: {
+    noExternal: ["lightningcss"],
   },
 });
